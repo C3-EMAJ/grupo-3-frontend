a1a6855f9f9b4ac43963f71efdb6d33f424bd9e5
tw7 <tiagogadenz@icloud.com>
2023-09-27 19:19:28 -0300

    Tela de Login

---
+++ src/components/Tela.js
@@ -0,0 +1,27 @@
+import './Tela.css';
+import imagem from "./emaj-login.png"
+
+
+function Tela() {
+    return(
+        <div id='tela' className='Tela'>
+           
+            <img src={imagem} id='imagem-login'></img>
+            
+            <p class="email-texto">Email</p>
+            <input class="input" type="text"></input>
+            
+            <p class="senha-texto">Password</p>
+            <input class="input" id='password' type='password'></input>
+            <div id='div-botao'>
+                <button id="botao"> <span id="texto-botao">Entrar</span> </button>
+            </div>
+            <div id='esqueci-senha'>
+                <a href="http://localhost:3000/"  target='_blank'> Esqueci minha senha </a>
+            </div> 
+
+        </div>
+    )
+}
+
+export default Tela;
